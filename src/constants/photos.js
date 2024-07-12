584d1a1efb18cf37e22c5538320d3619287b9340
Luminoso01 <novoaavila16@gmail.com>
2024-07-12 15:56:14 -0500

    change app title and alt text of images

--- src/constants/photos.js
+++ src/constants/photos.js
@@ -2,71 +2,71 @@ const photos = [
   {
     id: 0,
     src: "https://i.ytimg.com/vi/k3jC00dNSRI/maxresdefault.jpg",
-    alt: "Puppy photo",
+    alt: "Cat showing its tongue",
   },
 
   {
     id: 1,
     src: "https://1.bp.blogspot.com/-qHDTxqZHjM4/T6P2YahKLDI/AAAAAAAAJaI/BE7H62pkRQ0/s1600/funny-animal-pictures-of-the-week-10.jpg",
-    alt: "Puppy photo",
+    alt: "Polar bear eating carros in a swimming pool",
   },
 
   {
     id: 2,
     src: "https://cdn.acidcow.com/pics/20180827/animals_01.jpg",
-    alt: "Puppy photo",
+    alt: "Cat sitting elegantly on a chair",
   },
 
   {
     id: 3,
     src: "https://3.bp.blogspot.com/-S3jZdvszOMU/TiaB4MM-bpI/AAAAAAAAA1c/0I8moS7ZAa8/s640/Funny+animals+016.jpg",
-    alt: "Puppy photo",
+    alt: "Husky dog staring at you",
   },
 
   {
     id: 4,
     src: "https://funnypicture.org/wallpaper/2015/05/funny-animals-wallpapers-26-free-wallpaper.jpg",
-    alt: "Puppy photo",
+    alt: "Dog wearing glasses and smoking",
   },
 
   {
     id: 5,
     src: "https://bouncymustard.com/wp-content/uploads/2021/04/1-Cute-smiling-animals.jpg",
-    alt: "Puppy photo",
+    alt: "Dog showing its friendly smile",
   },
 
   {
     id: 6,
     src: "https://www.dumpaday.com/wp-content/uploads/2015/08/the-funny-animals-1.jpg",
-    alt: "Puppy photo",
+    alt: "Llama mocking you",
   },
   {
     id: 7,
     src: "https://4.bp.blogspot.com/-4nOrx8dHHyc/Wqv4G73PY_I/AAAAAAAB5B4/RxOn-hTiukE5KoQTlfQDmjO77IrsO8UQwCLcBGAs/s1600/funny-animals-308-01.jpg",
-    alt: "Puppy photo",
+    alt: "hedgehog wearing socks",
   },
 
   {
     id: 8,
     src: "https://1.bp.blogspot.com/-pIRF1I10Zhs/TyKNAaGoaBI/AAAAAAAABnk/eh8NaV05iiA/s1600/funny_animals_0.jpg",
-    alt: "Puppy photo",
+    alt: "Crazy Goat",
   },
 
   {
     id: 9,
     src: "https://www.dumpaday.com/wp-content/uploads/2018/07/funny-animals-1-4.jpg",
-    alt: "Puppy photo",
+    alt: "Meme of dog with cheetos on his face",
   },
 
   {
     id: 10,
     src: "https://izismile.com/img/img2/20090720/animals_90_01.jpg",
-    alt: "Puppy photo",
+    alt: "Dog imitating a duck with chips ",
   },
   {
     id: 11,
     src: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chipmunk-nature-photos-1537973822.jpg",
-    alt: "Puppy photo",
+    alt: "Squirrel eating cob",
   },
   {
     id: 12,
